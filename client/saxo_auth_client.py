5439cc615d45269960f134ecf5908234f7d5cf01
rniveau <romain.niveau@gmail.com>
2023-05-02 22:16:34 +0200

    chore: add and run black

--- client/saxo_auth_client.py
+++ client/saxo_auth_client.py
@@ -10,25 +10,36 @@ from typing import Dict, List
 
 SAXO_AUTH_URL = "https://sim.logonvalidation.net/"
 
-class SaxoAuthClient:
 
-    def __init__(self, configuration : Configuration) -> None:
+class SaxoAuthClient:
+    def __init__(self, configuration: Configuration) -> None:
         self.session = requests.Session()
-        self.session.headers.update({"Content-Type": "application/x-www-form-urlencoded"})
+        self.session.headers.update(
+            {"Content-Type": "application/x-www-form-urlencoded"}
+        )
         self.session.headers.update({"Host": "sim.logonvalidation.net"})
 
         self.configuration = configuration
 
     def login(self) -> str:
-        response = self.session.get(f"{SAXO_AUTH_URL}/authorize?response_type=code&client_id={self.configuration.app_key}"
-                         "&state=y90dsygas98dygoidsahf8sa&redirect_uri=http%3A%2F%2Flocalhost", allow_redirects=False)
+        response = self.session.get(
+            f"{SAXO_AUTH_URL}/authorize?response_type=code&client_id={self.configuration.app_key}"
+            "&state=y90dsygas98dygoidsahf8sa&redirect_uri=http%3A%2F%2Flocalhost",
+            allow_redirects=False,
+        )
         response.raise_for_status()
         return response.headers["Location"]
-    
-    def access_token(self, code : str) -> str:
-        auth_str = base64.b64encode(f"{self.configuration.app_key}:{self.configuration.app_secret}".encode('utf-8')).decode('utf-8')
-        response = self.session.post(f"{SAXO_AUTH_URL}token", data=f"grant_type=authorization_code&code={code}&redirect_uri=http%3A%2F%2Flocalhost",
-            headers={"Authorization": f"Basic {auth_str}"})
+
+    def access_token(self, code: str) -> str:
+        auth_str = base64.b64encode(
+            f"{self.configuration.app_key}:{self.configuration.app_secret}".encode(
+                "utf-8"
+            )
+        ).decode("utf-8")
+        response = self.session.post(
+            f"{SAXO_AUTH_URL}token",
+            data=f"grant_type=authorization_code&code={code}&redirect_uri=http%3A%2F%2Flocalhost",
+            headers={"Authorization": f"Basic {auth_str}"},
+        )
         response.raise_for_status()
         return response.json()["access_token"]
-        
--- utils/configuration.py
+++ utils/configuration.py
@@ -1,18 +1,19 @@
 import os
 import yaml
 
+
 class Configuration:
     def __init__(self):
         with open("secrets.yml", "r") as f:
             self.config = yaml.safe_load(f)
         if os.path.isfile("access_token"):
-            with open("access_token", 'r') as f:
+            with open("access_token", "r") as f:
                 self.access_token = f.read().strip()
 
     @property
     def app_key(self) -> str:
         return self.config["app_key"]
-    
+
     @property
     def app_secret(self) -> str:
         return self.config["app_secret"]
