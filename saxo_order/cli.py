99ebdf8aa5e68bf2c5056b92ae75201f031ef75d
rniveau <romain.niveau@gmail.com>
2023-05-06 19:27:55 +0200

    feat(#7): Add set-stop-limit-order command

--- saxo_order/cli.py
+++ saxo_order/cli.py
@@ -64,6 +64,36 @@ def set_order(price, code, country_code, quantity, order_type, buy_or_sell):
 
 
 @click.command()
+@click.option("--limit-price", type=float, required=True, help="The limit price of the order")
+@click.option("--stop-price", type=float, required=True, help="The stop price of the order")
+@click.option("--code", type=str, required=True, help="The code of the stock")
+@click.option(
+    "--country-code",
+    type=str,
+    required=True,
+    default="xpar",
+    help="The country code of the stock",
+)
+@click.option(
+    "--quantity", type=int, required=True, help="The wanted quantity of stocks"
+)
+@catch_exception(handle=SaxoException)
+def set_stop_limit_order(limit_price, stop_price, code, country_code, quantity):
+    client = SaxoClient(Configuration())
+    stock = client.get_stock(code=code, market=country_code)
+    client.set_order(
+        account_key="pblq5VW3dXTuCost|ihUfw==",
+        price=limit_price,
+        stop_price=stop_price,
+        quantiy=quantity,
+        order="stoplimit",
+        direction="buy",
+        stock_code=stock["Identifier"],
+    )
+
+
+
+@click.command()
 @click.option("--write", type=click.Choice(["y", "n"]), help="Write the access token ?")
 @catch_exception(handle=SaxoException)
 def auth(write):
