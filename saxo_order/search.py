525edb3ed5ae1a4869f50af8d96ea79ed52d3a76
rniveau <romain.niveau@gmail.com>
2023-08-01 22:50:57 +0200

    fix: search command

--- saxo_order/search.py
+++ saxo_order/search.py
@@ -5,9 +5,6 @@ from utils.configuration import Configuration
 from utils.exception import SaxoException
 from saxo_order import (
     catch_exception,
-    select_account,
-    validate_buy_order,
-    update_order,
     config_option,
 )
 from model import Order, Direction
@@ -23,7 +20,7 @@ from model import Order, Direction
     prompt="What is the keyword ?",
 )
 @catch_exception(handle=SaxoException)
-def search(config: str, search: str, asset_type: str):
+def search(config: str, search: str):
     client = SaxoClient(Configuration(config))
     for data in client.search(keyword=search):
         print(
