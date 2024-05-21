e483766cb37ea158a914790437b188e7790a3f5a
kiva <rniveau@alkemics.com>
2024-05-21 17:48:54 +0200

    chore: use dataclass in place of __init__

--- model/__init__.py
+++ model/__init__.py
@@ -6,76 +6,48 @@ from model.workflow import *
 from model.zone_bourse import ZoneBourseScore, ZoneBourseScrap
 
 
+@dataclass
 class Taxes:
-    def __init__(self, cost: float, taxes: float) -> None:
-        self.cost = cost
-        self.taxes = taxes
+    cost: float
+    taxes: float
 
 
+@dataclass
 class Account:
-    def __init__(
-        self,
-        key: str,
-        name: str,
-        fund: float = 0,
-        available_fund: float = 0,
-        client_key: str = "",
-    ) -> None:
-        self.key = key
-        self.name = name
-        self.fund = fund
-        self.available_fund = available_fund
-        self.client_key = client_key
+
+    key: str
+    name: str
+    fund: float = 0
+    available_fund: float = 0
+    client_key: str = ""
 
 
+@dataclass
 class Underlying:
-    def __init__(
-        self,
-        price: float,
-        stop: float = 0,
-        objective: float = 0,
-    ) -> None:
-        self.price = price
-        self.stop = stop
-        self.objective = objective
+    price: float
+    stop: float = 0
+    objective: float = 0
 
 
+@dataclass
 class Order:
-    def __init__(
-        self,
-        code: str,
-        price: float,
-        name: str = "",
-        quantity: float = 0,
-        objective: Optional[float] = None,
-        stop: Optional[float] = None,
-        comment: Optional[str] = None,
-        strategy: Optional[Strategy] = None,
-        direction: Optional[Direction] = None,
-        asset_type: str = "Stock",
-        type: OrderType = OrderType.LIMIT,
-        taxes: Optional[Taxes] = None,
-        underlying: Optional[Underlying] = None,
-        conditional: bool = False,
-        currency: Currency = Currency.EURO,
-        signal: Optional[Signal] = None,
-    ) -> None:
-        self.code = code
-        self.price = price
-        self.name = name
-        self.quantity = quantity
-        self.stop = stop
-        self.objective = objective
-        self.comment = comment
-        self.strategy = strategy
-        self.direction = direction
-        self.asset_type = asset_type
-        self.type = type
-        self.taxes = taxes
-        self.underlying = underlying
-        self.conditional = conditional
-        self.currency = currency
-        self.signal = signal
+
+    code: str
+    price: float
+    name: str = ""
+    quantity: float = 0
+    objective: Optional[float] = None
+    stop: Optional[float] = None
+    comment: Optional[str] = None
+    strategy: Optional[Strategy] = None
+    direction: Optional[Direction] = None
+    asset_type: str = "Stock"
+    type: OrderType = OrderType.LIMIT
+    taxes: Optional[Taxes] = None
+    underlying: Optional[Underlying] = None
+    conditional: bool = False
+    currency: Currency = Currency.EURO
+    signal: Optional[Signal] = None
 
     def __repr__(self) -> str:
         return f"{self.code}: {self.quantity} * {self.price} {self.currency}"
@@ -127,21 +99,19 @@ class ReportOrder(Order):
         self.open_position = open_position
 
 
+@dataclass
 class ConditionalOrder:
-    def __init__(
-        self, saxo_uic: int, trigger: TriggerOrder, price: float, asset_type: str
-    ) -> None:
-        self.saxo_uic = saxo_uic
-        self.trigger = trigger
-        self.price = price
-        self.asset_type = asset_type
+    saxo_uic: int
+    trigger: TriggerOrder
+    price: float
+    asset_type: str
 
 
+@dataclass
 class StackingReport:
-    def __init__(self, date: str, asset: str, quantity: float) -> None:
-        self.date = date
-        self.asset = asset
-        self.quantity = quantity
+    date: str
+    asset: str
+    quantity: float
 
     @property
     def id(self) -> str:
