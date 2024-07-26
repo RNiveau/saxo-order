4c26741ee1c232d58357a152ea4b6aca20a8e73b
kiva <rniveau@alkemics.com>
2024-07-26 10:37:06 +0200

    fix: handle all errors in the lambda launcher and send them to slack

--- lambda_function.py
+++ lambda_function.py
@@ -10,25 +10,27 @@ from utils.exception import SaxoException
 
 
 def handler(event, _):
-    match event.get("command"):
-        case "refresh_token":
-            configuration = Configuration(os.getenv("SAXO_CONFIG"))
-            auth_client = SaxoAuthClient(configuration)
-            try:
+    try:
+        match event.get("command"):
+            case "refresh_token":
+                configuration = Configuration(os.getenv("SAXO_CONFIG"))
+                auth_client = SaxoAuthClient(configuration)
                 access_token, refresh_token = auth_client.refresh_token()
-            except Exception as e:
-                slack_client = WebClient(token=configuration.slack_token)
-                slack_client.chat_postMessage(
-                    channel="#errors",
-                    text=f"Refresh token fails:\n```{e}```",
+                configuration.save_tokens(access_token, refresh_token)
+                return {"result": "ok", "message": "token has been refreshed"}
+            case "alerting":
+                run_alerting(os.getenv("SAXO_CONFIG"))
+            case "workflows":
+                execute_workflow(os.getenv("SAXO_CONFIG"))
+            case _:
+                raise SaxoException(
+                    f"Command {event.get('command')} not found"
                 )
-                return {"result": "ko", "message": "can't refresh the token"}
-            configuration.save_tokens(access_token, refresh_token)
-            return {"result": "ok", "message": "token has been refreshed"}
-        case "alerting":
-            run_alerting(os.getenv("SAXO_CONFIG"))
-        case "workflows":
-            execute_workflow(os.getenv("SAXO_CONFIG"))
-        case _:
-            raise SaxoException(f"Command {event.get('command')} not found")
+    except Exception as e:
+        slack_client = WebClient(token=configuration.slack_token)
+        slack_client.chat_postMessage(
+            channel="#errors",
+            text=f"Command {event.get('command')} fails:\n```{e}```",
+        )
+        return {"result": "ko", "message": "can't refresh the token"}
     return {"result": "ok"}
