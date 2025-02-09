054b40e417153e24b739a88966ac636037d7a528
Kiva <romain.niveau@gmail.com>
2025-02-09 17:44:01 +0100

    feat: test indicator to understand if we have a convergence

--- saxo_order/commands/snapshot.py
+++ saxo_order/commands/snapshot.py
@@ -1,3 +1,4 @@
+from typing import List
 import click
 from click.core import Context
 from prettytable import PrettyTable
@@ -75,45 +76,51 @@ def execute_snapshot(config: str):
         bb_h1_25 = bollinger_bands(candles_h1, 2.5)
         bb_h4_25 = bollinger_bands(candles_h4, 2.5)
         bb_daily_25 = bollinger_bands(candles_daily, 2.5)
+        am50_h1 = mobile_average(candles=candles_h1, period=50)
+        am50_h4 = mobile_average(candles=candles_h4, period=50)
+        am50_daily = mobile_average(candles=candles_daily, period=50)
         table.field_names = ["Indicator", "H1", "H4", "Daily"]
+        zones = find_zones([am50_h1, am50_h4, am50_daily, bb_h1_2.bottom, bb_h4_2.bottom, bb_daily_2.bottom, bb_h1_25.bottom, bb_h4_25.bottom, bb_daily_25.bottom, bb_h1_2.up, bb_h4_2.up, bb_daily_2.up, bb_h1_25.up, bb_h4_25.up, bb_daily_25.up])
+        # next step: deflatten the zones to show a different symbol.
+        # create a method to show the value
         table.add_row(
             [
                 "MM 50",
-                f"{mobile_average(candles=candles_h1, period=50):.2f}",
-                f"{mobile_average(candles=candles_h4, period=50):.2f}",
-                f"{mobile_average(candles=candles_daily, period=50):.2f}",
+                f"{am50_h1:.2f} {'*' if am50_h1 in zones else ''}",
+                f"{am50_h4:.2f} {'*' if am50_h4 in zones else ''}",
+                f"{am50_daily:.2f} {'*' if am50_daily in zones else ''}",
             ]
         )
         table.add_row(
             [
                 "BBH 2.0",
-                f"{bb_h1_2.up:.2f}",
-                f"{bb_h4_2.up:.2f}",
-                f"{bb_daily_2.up:.2f}",
+                f"{bb_h1_2.up:.2f} {'*' if bb_h1_2.up in zones else ''}",
+                f"{bb_h4_2.up:.2f} {'*' if bb_h4_2.up in zones else ''}",
+                f"{bb_daily_2.up:.2f} {'*' if bb_daily_2.up in zones else ''}"
             ]
         )
         table.add_row(
             [
                 "BBH 2.5",
-                f"{bb_h1_25.up:.2f}",
-                f"{bb_h4_25.up:.2f}",
-                f"{bb_daily_25.up:.2f}",
+                f"{bb_h1_25.up:.2f} {'*' if bb_h1_25.up in zones else ''}",
+                f"{bb_h4_25.up:.2f} {'*' if bb_h4_25.up in zones else ''}",
+                f"{bb_daily_25.up:.2f} {'*' if bb_daily_25.up in zones else ''}",
             ]
         )
         table.add_row(
             [
                 "BBL 2.0",
-                f"{bb_h1_2.bottom:.2f}",
-                f"{bb_h4_2.bottom:.2f}",
-                f"{bb_daily_2.bottom:.2f}",
+                f"{bb_h1_2.bottom:.2f} {'*' if bb_h1_2.bottom in zones else ''}",
+                f"{bb_h4_2.bottom:.2f} {'*' if bb_h4_2.bottom in zones else ''}",
+                f"{bb_daily_2.bottom:.2f} {'*' if bb_daily_2.bottom in zones else ''}",
             ]
         )
         table.add_row(
             [
                 "BBL 2.5",
-                f"{bb_h1_25.bottom:.2f}",
-                f"{bb_h4_25.bottom:.2f}",
-                f"{bb_daily_25.bottom:.2f}",
+                f"{bb_h1_25.bottom:.2f} {'*' if bb_h1_25.bottom in zones else ''}",
+                f"{bb_h4_25.bottom:.2f} {'*' if bb_h4_25.bottom in zones else ''}",
+                f"{bb_daily_25.bottom:.2f} {'*' if bb_daily_25.bottom in zones else ''}",
             ]
         )
         slack_client.chat_postMessage(
@@ -121,3 +128,20 @@ def execute_snapshot(config: str):
             text=f"{index} ({candles_h1[0].close}) ```{table}```",
         )
         print(table)
+
+def find_zones(numbers: List[float]) -> List[List[float]]:
+    sorted_numbers = sorted(numbers)
+    zones = []
+    current_zone = [sorted_numbers[0]]
+
+    for i in range(1, len(sorted_numbers)):
+        if sorted_numbers[i] - current_zone[-1] <= 10:
+            current_zone.append(sorted_numbers[i])
+        else:
+            zones.append(current_zone)
+            current_zone = [sorted_numbers[i]]
+    
+    if current_zone:
+        zones.append(current_zone)
+    
+    return list(filter(lambda x: len(x) > 1, zones))
